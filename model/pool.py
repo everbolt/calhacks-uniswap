127924bf9ebaf2a815b382dd4e32396f39584a3b
Elden Ren <eldenbr2015@gmail.com>
2021-10-23 12:26:29 -0700

    wrote initial pool logic

--- model/pool.py
+++ model/pool.py
@@ -4,6 +4,8 @@ class Pool:
     def __init__(self, liquidity_concentration, sqrtPrice, liquidity, curTick):
         self.liquidity_concentration = liquidity_concentration
         self.sqrtPrice = sqrtPrice
+        self.liquidity = liquidity
+        self.tick = curTick
 
     def verify_liquidity(quantity0, quantity1, minPrice0, maxPrice0):
         return True
@@ -15,17 +17,25 @@ class Pool:
     def swap(self, deltaX):
         if deltaX < 0:
             if abs(deltaX) > self.liquidity_concentration[self.tick]/self.liquidity_concentration[self.tick].pa - self.liquidity/self.sqrtPrice:
-                deltaX += self.liquidity_concentration[self.tick]/self.liquidity_concentration[self.tick].pa - self.liquidity/self.sqrtPrice
-                self.sqrtPrice = self.liquidity_concentration[self.tick]/self.liquidity_concentration[self.tick].pa
+                deltaX += self.liquidity_concentration[self.tick].L/self.liquidity_concentration[self.tick].pa - self.liquidity/self.sqrtPrice
+                self.sqrtPrice = self.liquidity_concentration[self.tick].L/self.liquidity_concentration[self.tick].pa
                 self.tick += 1
+                self.liquidity = self.liquidity_concentration[self.tick].L
                 self.swap(deltaX)
             else:
-                # solve formula
+                curX = self.liquidity/self.sqrtPrice
+                curX += deltaX
+                curY = self.liquidity/(curX+self.liquidity/self.liquidity_concentration[self.tick].pb)-self.liquidity/self.liquidity_concentration[self.tick].pa
+                self.sqrtPrice = np.sqrt(curY/curX)
         else:
-            if deltaX > self.liquidity/self.sqrtPrice-self.liquidity_concentration[self.tick]/self.liquidity_concentration[self.tick].pb:
-                deltaX -= self.liquidity_concentration[self.tick]/self.liquidity_concentration[self.tick].pa - self.liquidity/self.sqrtPrice
+            if deltaX > self.liquidity/self.sqrtPrice-self.liquidity_concentration[self.tick].L/self.liquidity_concentration[self.tick].pb:
+                deltaX -= self.liquidity/self.sqrtPrice-self.liquidity_concentration[self.tick].L/self.liquidity_concentration[self.tick].pb
                 self.sqrtPrice = self.liquidity_concentration[self.tick]/self.liquidity_concentration[self.tick].pb
                 self.tick -= 1
+                self.liquidity = self.liquidity_concentration[self.tick].L
                 self.swap(deltaX)
             else:
-                # solve formula
+                curX = self.liquidity/self.sqrtPrice
+                curX += deltaX
+                curY = self.liquidity/(curX+self.liquidity/self.liquidity_concentration[self.tick].pb)-self.liquidity/self.liquidity_concentration[self.tick].pa
+                self.sqrtPrice = np.sqrt(curY/curX)
