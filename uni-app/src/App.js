a403bf72d3bfafc8e669cd426dda7d880d3b9b33
Tobias Worledge <tobiasworledge@gmail.com>
2021-10-23 12:01:09 -0700

    Added token_names and cleaned up other data

--- uni-app/src/App.js
+++ uni-app/src/App.js
@@ -4,12 +4,8 @@ import './App.css';
 import LineChart from './Components/LineChart';
 import MinimumDistanceSliderWithInput from './Components/MinSliderWithInput';
 import Calculator from './Components/CryptoConverter'
-import tokens from './Components/Data/tokens.js'
 
 const App = () => {
-  
-  console.log(JSON.stringify(tokens))
-  
   return (
     <div>
       <LineChart />
