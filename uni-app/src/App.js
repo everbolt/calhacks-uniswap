d4d201f45222e34b85f46fa4a2c1d665e8f75828
yuezy1234 <yuezy2015@gmail.com>
2021-10-23 10:39:56 -0700

    Adapted a temperature converter for calculations. Not customizable atm.

--- uni-app/src/App.js
+++ uni-app/src/App.js
@@ -2,15 +2,15 @@ import React from 'react';
 
 import './App.css';
 import LineChart from './Components/LineChart';
-import MinimumDistanceSlider from './Components/MinSlider';
-import InputSlider from './Components/InputSlider';
 import MinimumDistanceSliderWithInput from './Components/MinSliderWithInput';
+import Calculator from './Components/CryptoConverter'
 
 const App = () => {
   return (
     <div>
       <LineChart />
       <MinimumDistanceSliderWithInput />
+      <Calculator />
     </div>
   )
 }
