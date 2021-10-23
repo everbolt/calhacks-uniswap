4957131bd568b5be8f95c11c55358b7e43a7a1fa
Tobias Worledge <tobiasworledge@gmail.com>
2021-10-23 11:55:04 -0700

    Added Data tokens and dir

--- uni-app/src/App.js
+++ uni-app/src/App.js
@@ -4,8 +4,12 @@ import './App.css';
 import LineChart from './Components/LineChart';
 import MinimumDistanceSliderWithInput from './Components/MinSliderWithInput';
 import Calculator from './Components/CryptoConverter'
+import tokens from './Components/Data/tokens.js'
 
 const App = () => {
+  
+  console.log(JSON.stringify(tokens))
+  
   return (
     <div>
       <LineChart />
---
+++ uni-app/src/Components/Data/tokens.js
@@ -0,0 +1,15 @@
+const tokens = {
+    "WETH": "0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2",
+    "USDC": "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48",
+    "WBTC": "0x2260fac5e5542a773aa44fbcfedf7c193bc2c599",
+    "USDT": "0xdac17f958d2ee523a2206206994597c13d831ec7",
+    "DAI": "0x6b175474e89094c44da98b954eedeac495271d0f",
+    "UNI": "0x1f9840a85d5af5bf1d1762f925bdaddc4201f984",
+    "FRAX": "0x853d955acef822db058eb8505911ed77f175b99e",
+    "PAX": "0x8e870d67f660d95d5be530380d0ec0bd388289e1",
+    "RBN": "0x6123b0049f904d730db3c36a31167d9d4121fa6b",
+    "RPL": "0xb4efd85c19999d84251304bda99e90b92300bd93",
+    "SHIB": "0x95ad61b0a150d79219dcf64e1e6cc01f0b64c4ce"
+}
+
+module.exports = {tokens}
