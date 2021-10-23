d60325ce4c7d5b94bdb228e807876afd0d8b7404
yuezy1234 <yuezy2015@gmail.com>
2021-10-22 21:42:14 -0700

    Filled in between 2 lines

--- uni-app/src/Components/LineChart.js
+++ uni-app/src/Components/LineChart.js
@@ -6,14 +6,21 @@ const LineChart = () => {
     <div>
       <Line 
         data = {{
-          labels: ['1','2', '3', '4', '5', '6'],
+          labels: ['1','2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12'],
           datasets: [
             {
               label: 'DataSet1',
-              data: [12, 19, 3, 5, 2, 3],
-              borderColor: "#bae755",
-              backgroundColor: "#e755ba",
-            }
+              data: [1, 5, 6, 16, 1, 5, 3, 7, 4, 8, 9, 3],
+              borderColor: "#f731ed",
+              backgroundColor: "#080554",
+              fill: +1,
+            }, 
+            {
+              label: 'DataSet2',
+              data: [12, 19, 9, 17, 2, 8, 7, 10, 5, 10, 11, 7],
+              borderColor: "#f731ed",
+              backgroundColor: "#080554",
+            }, 
           ]
         }}
         
@@ -22,7 +29,7 @@ const LineChart = () => {
             mode: 'index',
             axis: 'x',
             intersect: false
-          }
+          },
         }}
       />
     </div>
