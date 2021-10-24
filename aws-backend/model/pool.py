9e9a5a67c745c12805cc1b23d8094921b375032a
Elden Ren <eldenbr2015@gmail.com>
2021-10-23 23:19:59 -0700

    fixed pool

--- aws-backend/model/pool.py
+++ aws-backend/model/pool.py
@@ -1,36 +1,72 @@
 import numpy as np
 
 class Pool:
-    def __init__(self, liquidity_concentration, sqrtPrice, liquidity, curTick):
+    def __init__(self, liquidity_concentration, sqrtPrice, liquidity, curTick, fee=0.003):
         self.liquidity_concentration = liquidity_concentration
         self.sqrtPrice = sqrtPrice
         self.liquidity = liquidity
         self.tick = curTick
         self.leftTrack = -1
         self.rightTrack = -1
+        self.fee = fee
+        self.fees_collectedX = 0
+        self.fees_collectedY = 0
+        self.liquidity_tracked = 0
 
     def verify_liquidity(quantity0, quantity1, minPrice0, maxPrice0):
         return True
 
     def add_liquidity(self, minPrice0, maxPrice0, deltaX, deltaY, track=True):
+        print(minPrice0)
+        print(maxPrice0)
+        print(deltaX)
+        print(deltaY)
         deltaL = None
+        pa = np.sqrt(minPrice0)
+        pb = np.sqrt(maxPrice0)
         if deltaX == 0:
-            deltaL = deltaY/(np.sqrt(maxPrice0)-np.sqrt(minPrice0))
+            deltaL = deltaY/(pb-pa)
         elif deltaY == 0:
-            deltaL = deltaX/(1/np.sqrt(minPrice0)-1/np.sqrt(maxPrice0))
+            deltaL = deltaX/(1/pa-1/pb)
         else:
-            deltaL = deltaX/(1/self.sqrtPrice - 1/np.sqrt(maxPrice0))
-            if abs(deltaL-deltaY/(self.sqrtPrice-np.sqrt(minPrice0))) > 0.01:
+            deltaL = deltaX/(1/self.sqrtPrice - 1/pb)
+            if abs(deltaL-deltaY/(self.sqrtPrice-pa)) > 0.1:
                 return
         if not deltaL:
             return
-        l = len(self.liquidity_concentration)
-        r = -1
-        for i in range(len(self.liquidity_concentration)):
-            if self.liquidity_concentration[i].prev >= np.sqrt(minPrice0) and self.liquidity_concentration[i].next <= np.sqrt(maxPrice0):
-                self.liquidity_concentration[i].L += deltaL
-                l = min(l, i)
-                r = max(r, i)
+        print(deltaL)
+        lo = 0
+        hi = len(self.liquidity_concentration)-1
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if self.liquidity_concentration[mid].prev < pa:
+                lo = mid + 1
+            elif self.liquidity_concentration[mid].prev > pa:
+                hi = mid - 1
+            else:
+                lo = mid
+                hi = mid
+        l = lo
+
+        lo = 0
+        hi = len(self.liquidity_concentration)
+        while lo < hi:
+            mid = (lo + hi) // 2
+            if self.liquidity_concentration[mid].prev < pb:
+                lo = mid + 1
+            elif self.liquidity_concentration[mid].prev > pb:
+                hi = mid
+            else:
+                lo = mid
+                hi = mid
+        r = lo
+        
+        self.liquidity_concentration[l].netL += deltaL
+        if r < len(self.liquidity_concentration):
+            self.liquidity_concentration[r].netL -= deltaL
+        if l <= self.tick and r > self.tick:
+            self.liquidity += deltaL
+
         if track:
             self.leftTrack = l
             self.rightTrack = r
@@ -39,32 +75,44 @@ class Pool:
             self.fees_collectedX = 0
             self.fees_collectedY = 0
             self.init_value_x = deltaX + deltaY/(self.sqrtPrice**2)
+            self.liquidity_tracked = deltaL
         
     def swap(self, deltaX):
         print("deltaX: " + str(deltaX))
         virtualX = self.liquidity/self.sqrtPrice
+        init_price = self.sqrtPrice
         if deltaX < 0:
             tickX = virtualX - self.liquidity/self.liquidity_concentration[self.tick].next
             if abs(deltaX) > tickX:
                 deltaX += tickX
                 self.sqrtPrice = self.liquidity_concentration[self.tick].next
+                deltaY = (init_price-self.sqrtPrice)*self.liquidity
+                if self.tick >= self.leftTrack and self.tick < self.rightTrack:
+                    self.fees_collectedY += self.fee*deltaY*(self.liquidity_tracked/self.liquidity)
                 self.tick += 1
-                self.liquidity = self.liquidity_concentration[self.tick].L
+                self.liquidity += self.liquidity_concentration[self.tick].netL
                 self.swap(deltaX)
             else:
                 virtualX += deltaX
                 virtualY = (self.liquidity/np.sqrt(virtualX))**2
                 self.sqrtPrice = np.sqrt(virtualY/virtualX)
+                deltaY = (init_price-self.sqrtPrice)*self.liquidity
+                if self.tick >= self.leftTrack and self.tick < self.rightTrack:
+                    self.fees_collectedY += self.fee*deltaY*(self.liquidity_tracked/self.liquidity)
                 deltaX = 0
         else:
             tickX = self.liquidity/self.liquidity_concentration[self.tick].prev-virtualX
             if deltaX > tickX:
                 deltaX -= tickX
+                if self.tick >= self.leftTrack and self.tick < self.rightTrack:
+                    self.fees_collectedX += tickX*(1/(1-self.fee)-1)*(self.liquidity_tracked/self.liquidity)
                 self.sqrtPrice = self.liquidity_concentration[self.tick].prev
                 self.tick -= 1
-                self.liquidity = self.liquidity_concentration[self.tick].L
+                self.liquidity -= self.liquidity_concentration[self.tick].netL
                 self.swap(deltaX)
             else:
+                if self.tick >= self.leftTrack and self.tick < self.rightTrack:
+                    self.fees_collectedX += deltaX*(1/(1-self.fee)-1)*(self.liquidity_tracked/self.liquidity)
                 virtualX += deltaX
                 virtualY = (self.liquidity/np.sqrt(virtualX))**2
                 self.sqrtPrice = np.sqrt(virtualY/virtualX)
@@ -76,5 +124,17 @@ class Pool:
     def getY(self):
         return self.liquidity*self.sqrtPrice - self.liquidity*self.liquidity_concentration[self.tick].prev
     
-    def value(self):
-        return self.getX() + self.getY()/(self.sqrtPrice**2)
+    def value(self, X, Y):
+        return X + Y/(self.sqrtPrice**2)
+
+    def coin_value(self):
+        return self.value(self.getX(), self.getY())
+    
+    def fee_value(self):
+        return self.value(self.fees_collectedX, self.fees_collectedY)
+
+    def total_value(self):
+        return self.fee_value() + self.coin_value()
+    
+    def init_value(self):
+        return self.init_value_x
--- aws-backend/model/test.py
+++ aws-backend/model/test.py
@@ -1,26 +1,30 @@
 from tick import Tick
 from pool import Pool
 from trade_distribution import create_trade_distribution
-from swap_parser import get_swaps
+from swap_parser import swapParser
 import numpy as np
 
-'''
-t = Tick(-1000, 1000, 5000000)
-t1 = Tick(-2000, -1000, 5000000)
+
+t = Tick(-1000, 1000, 0, 0, 5000000)
+#t1 = Tick(-2000, -1000, 5000000)
 liquidity_concentration = [t]
 p = Pool(liquidity_concentration, 1, 5000000, 0)
 print(p.getX())
 print(p.getY())
-p.swap(-50)
+deltaX = 2000
+deltaL = deltaX/(1 - 1/t.next)
+deltaY = deltaL * (1-t.prev)
+p.add_liquidity(t.prev**2, t.next**2, deltaX, deltaY)
 print(p.getX())
 print(p.getY())
-
 p.swap(50)
 print(p.getX())
 print(p.getY())
-'''
+print(p.coin_value())
+print(p.fee_value())
 
-dist = get_swaps()
+'''
+dist = swapParser()
 print(sum(dist)/len(dist))
 np.random.shuffle(dist)
 #print(dist)
@@ -29,3 +33,4 @@ trade_dist = create_trade_distribution(dist)
 sample = trade_dist(100)
 print(sample)
 print(sum(sample)/len(sample))
+'''
