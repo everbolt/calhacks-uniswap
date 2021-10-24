a6b96ba4db774b4ed00d3db130600473d5baaecc
Elden Ren <eldenbr2015@gmail.com>
2021-10-24 03:34:58 -0700

    added fee reading

--- aws-backend/model/pool_parser.py
+++ aws-backend/model/pool_parser.py
@@ -57,6 +57,7 @@ def createPool(data_tick, data_pool, data_token):
     ret_pool = pool.Pool(liquidity_concentration,
         int(data_pool['sqrtPrice']) / (2 ** 96 * 10 ** ((decimals_dic[data_pool['token1']['id']] - decimals_dic[data_pool['token0']['id']]) / 2)),
         int(data_pool['liquidity']) / 10 ** ((decimals_dic[data_pool['token0']['id']] + decimals_dic[data_pool['token1']['id']]) / 2),
-        curr_tick)
+        curr_tick,
+        fee=int(data_pool['feeTier'])/1000000)
     
     return ret_pool
