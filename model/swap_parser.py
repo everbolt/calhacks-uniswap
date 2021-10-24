2669e38f2f3073a98c420c82d2853b59ec24b549
Elden Ren <eldenbr2015@gmail.com>
2021-10-23 19:34:12 -0700

    tested trade_distribution, it's not terrible?

--- model/swap_parser.py
+++ model/swap_parser.py
@@ -1,13 +1,18 @@
 import json
 import swap
 import math
+import numpy as np
 
-f = open('model/Trade-res.json')
+f = open('./Trade-res.json')
 data = json.load(f)['data']['pool']
 
 swap_distribution = []
 
 for curr_swap in data['swaps']:
-    swap_distribution += [swap.Swap(float(curr_swap['amount0']), float(curr_swap['amount1']), int(curr_swap['timestamp']))]
+    #swap_distribution += [swap.Swap(float(curr_swap['amount0']), float(curr_swap['amount1']), int(curr_swap['timestamp']))]
+    swap_distribution.append(float(curr_swap['amount0']))
 
-print(swap_distribution)
+def get_swap_dist():
+    return np.array(swap_distribution)
+
+#print(swap_distribution)
